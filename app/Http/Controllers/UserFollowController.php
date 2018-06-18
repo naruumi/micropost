55a0815591349d5f872141d6a512d448095c51e2
EC2 Default User <ec2-user@ip-172-31-19-2.us-east-2.compute.internal>
2018-06-18 06:24:42 +0000

    Procfile

---
+++ app/Http/Controllers/UserFollowController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserFollowController extends Controller
+{
+    public function store(Request $request, $id)
+    {
+        \Auth::user()->follow($id);
+        return redirect()->back();
+    }
+
+    public function destroy($id) 
+    {
+        \Auth::user()->unfollow($id);
+        return redirect()->back();
+    }
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+    }
+
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
+    }
+}
